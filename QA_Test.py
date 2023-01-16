26edd4626256bbb2bb045d2172aa5d6ffc8b596b
Kevin Khew <73118508+Villicus-np@users.noreply.github.com>
2023-01-16 13:50:51 +0800

    Update QA_Test.py

--- QA_Test.py
+++ QA_Test.py
@@ -2,4 +2,4 @@ import pytest
 
 def test_successString():
   result1 = helloworld("item")
-  assert result1 == "Hello item! Welcome to Hello World File!"
+  assert result1 == "Hello item! Welcome to Hello"
