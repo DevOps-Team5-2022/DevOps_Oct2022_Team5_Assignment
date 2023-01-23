be5b2833b88653cd65a5cc4b4a931dc3862867d8
TJJ101 <S10194152@connect.np.edu.sg>
2023-01-24 02:44:23 +0800

    Added a comment

--- main.py
+++ main.py
@@ -8,4 +8,5 @@ def hello_world():
     return render_template("home.html")
 
 if __name__ == '__main__':
+    #host is to set the localhost IP Address, port is to set the port
     app.run(host = '127.0.0.1', port ='5221', debug = True)
