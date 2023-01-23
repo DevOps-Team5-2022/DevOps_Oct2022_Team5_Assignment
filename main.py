95baa139e759ebfe8fcd240d77cc599132098f51
TJJ101 <S10194152@connect.np.edu.sg>
2023-01-24 02:41:56 +0800

    Change the main.py to render home.html in templates folder

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/Main")
 
 def hello_world():
-    return "Hello World!"
+    return render_template("home.html")
 
 if __name__ == '__main__':
     app.run(host = '127.0.0.1', port ='5221', debug = True)
