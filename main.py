e1b833a678469e5de41b415629f7d889da057212
TJJ101 <S10194152@connect.np.edu.sg>
2023-01-24 13:44:45 +0800

    Added Navigation to new pages

--- main.py
+++ main.py
@@ -3,9 +3,24 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/Main")
-
-def hello_world():
+def home():
     return render_template("home.html")
+
+@app.route("/Upload_Data")
+def upload_data():
+    return render_template("upload_data.html")
+
+@app.route("/Match_Student")
+def match_student():
+    return render_template("match_student.html")
+
+@app.route("/Prepare_Email")
+def prepare_email():
+    return render_template("prepare_email.html")
+
+@app.route("/Settings")
+def settings():
+    return render_template("settings.html")
 
 if __name__ == '__main__':
     #host is to set the localhost IP Address, port is to set the port
