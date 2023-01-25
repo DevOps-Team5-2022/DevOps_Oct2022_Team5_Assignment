c8cfae062158940d8360a497e33ede7176d18e39
TheBetterSamuel <73062796+TheBetterSamuel@users.noreply.github.com>
2023-01-25 21:26:46 +0800

    Added negative code for the Uploading Student Data test

--- QA_Test.py
+++ QA_Test.py
@@ -95,9 +95,80 @@ def test_uploadStudentData():
     for row in totalRows:
         student_name = row.findElements(By.id("student-name"))
         student_id = row.findElements(By.id("student-id"))
+        #this should be the expected result
         if student_name == studentName && student_id == studentID:
             dataFound = True
             break;
+    if dataFound == False:
+        print("Error! Student Data was not uploaded successfully!")
+
+    driver.quit()
+
+#test if student data file is wrong, an error message would show and the data should not be imported inside the database or be displayed
+def test_uploadStudentData_Invalid():
+    driver = webdriver.Chrome(service=ChromeService(executable_path=ChromeDriverManager().install()))
+
+    driver.get(siteIPAddress)
+
+    #checks if the home pahe is loaded
+    title = driver.title
+    assert title == "Home"
+
+    driver.implicitly_wait(0.5)
+
+    #find the button to enter Upload Data page, subdue to naming changes
+    upload_button = driver.find_element(by=By.ID, value="upload-data-button")
+    
+    #Upload Data button is clicked
+    upload_button.click()
+
+    driver.implicitly_wait(0.5)
+    
+    #checks if the Upload Data pahe is loaded
+    target_title = driver.title
+    assert target_title == "Upload Data"
+
+    driver.implicitly_wait(0.5)
+
+    #find the upload file button and click it
+    upload_student_data_button = driver.find_element(by=By.ID, value="upload-student-data-file")
+    
+    #Upload Student Data button is clicked
+    upload_student_data_button.send_keys("invalid-student-data.xml")
+
+    #check if the right file is uploaded
+    file_upload_message = driver.find_element(by=By.ID, value="upload-student-data-error-message").text
+    assert file_upload_message + "Error uploading student data"
+
+    driver.implicitly_wait(2)
+
+    #find the button to enter Upload Data page, subdue to naming changes
+    view_students_button = driver.find_element(by=By.ID, value="view-students-button")
+    
+    #Upload Data button is clicked
+    view_students_button.click()
+
+    driver.implicitly_wait(0.5)
+    
+    #checks if the Upload Data pahe is loaded
+    target_title2 = driver.title
+    assert target_title2 == "View Students"
+
+    driver.implicitly_wait(0.5)
+
+    student_table = driver.findElement(By.id("View"));
+    #get the size of the list of the rows
+    totalRows = student_table.findElements(By.tagName("tr"))
+
+    dataFound = False
+    #find the student data, if found, set dataFound to true
+    for row in totalRows:
+        student_name = row.findElements(By.id("student-name"))
+        student_id = row.findElements(By.id("student-id"))
+        if student_name == studentName && student_id == studentID:
+            dataFound = True
+            break;
+    #This should be the expected result
     if dataFound == False:
         print("Error! Student Data was not uploaded successfully!")
 
