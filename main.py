b8743800457fcd0d248096139786fc408b42f04d
TJJ101 <S10194152@connect.np.edu.sg>
2023-01-29 02:27:47 +0800

    Made changes to Settings page to handle POST to get inputed path directory

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import os
 
+emailDirPath = ""
+resumeDirPath = ""
+    
 app = Flask(__name__)
 
 @app.route("/Main")
@@ -18,9 +22,26 @@ def match_student():
 def prepare_email():
     return render_template("prepare_email.html")
 
-@app.route("/Settings")
+@app.route("/Settings", methods=['POST', 'GET'])
 def settings():
-    return render_template("settings.html")
+    global emailDirPath
+    global resumeDirPath
+    if request.method == 'POST':
+        if 'submit-email-dir-btn' in request.form:
+            emailDirPath = request.form.get("input-email-dir")
+            #if check_path_exists(emailDirPath):
+                # do smth
+        elif 'submit-resume-dir-btn' in request.form:
+            resumeDirPath = request.form.get("input-resume-dir")
+            #if check_path_exists(resumeDirPath):
+                # do smth
+
+    return render_template("settings.html", emailPath = emailDirPath, resumePath = resumeDirPath)
+
+# function that checks if directory exists and returns bool 
+def check_path_exists(directory):
+    check = os.path.isdir(directory)
+    return check
 
 if __name__ == '__main__':
     #host is to set the localhost IP Address, port is to set the port
