e6acc7cd2d3052136c2b10526e8bc4680f366337
TJJ101 <S10194152@connect.np.edu.sg>
2023-02-03 20:58:52 +0800

    Added function to upload student and company data into database

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 import os
 import Email
 import mysql.connector
+import pandas as pd
+from sqlalchemy import create_engine
 
 emailDirPath = ""
 resumeDirPath = ""
 conn = mysql.connector.connect(user='root', password='root',
                               host='localhost',database='devops')
 
+# this is for uploading excel data into the sql db, mysql.connector doesnt work
+engine = create_engine("mysql://{user}:{pw}@{host}/{db}"
+				.format(host='localhost', db='devops', user='root', pw='root'))
+
+
+
 cursor = conn.cursor()
 app = Flask(__name__)
+
+# required for using flash()
+app.config['SECRET_KEY'] = os.urandom(24)
 
 @app.route("/Main")
 def home():
@@ -17,10 +28,31 @@ def home():
 
 @app.route("/Upload_Data", methods=['POST', 'GET'])
 def upload_data():
+    msg = ''
+    sDataSuccess = False
+    cDataSuccess = False
     if request.method == 'POST':
         studentData = request.files['student-data-upload']
         companyData = request.files['company-data-upload']
-    return render_template("upload_data.html")
+        print(studentData)
+        if studentData.filename != '':
+            studentDataDF = pd.read_excel(studentData)
+            msg = upload_data_func(studentDataDF, 'student')
+            if msg == 'success':
+                sDataSuccess = True
+            else:
+                sDataSuccess = False
+            
+        if companyData.filename != '':
+            companyDataDF = pd.read_excel(companyData)
+            msg = upload_data_func(companyDataDF, 'company')
+            if msg == 'success':
+                cDataSuccess = True
+            else:
+                cDataSuccess = False
+
+        
+    return render_template("upload_data.html", result = msg, success1 = sDataSuccess, success2 = cDataSuccess)
 
 @app.route("/Match_Student")
 def match_student():
@@ -54,6 +86,19 @@ def settings():
 def check_path_exists(directory):
     check = os.path.isdir(directory)
     return check
+
+# function to try to upload data to database
+def upload_data_func(file, tableName):
+    try:
+        file.to_sql(tableName, engine, if_exists='append', index = False)
+    except:
+        # error with uploading
+        return 'error'
+        flash('Invalid Table Format', 'error')
+    else:
+        # success with uploading
+        return 'success'
+        flash('Upload Successful!')
 
 if __name__ == '__main__':
     #host is to set the localhost IP Address, port is to set the port
