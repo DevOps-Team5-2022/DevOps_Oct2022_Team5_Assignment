b76a833bc39dc12995e88ec37ab587e53490fbfd
TJJ101 <S10194152@connect.np.edu.sg>
2023-02-04 14:52:32 +0800

    Trying to fix Selenium issue in CI

--- Dev_Test.py
+++ Dev_Test.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome import options
 from webdriver_manager.chrome import ChromeDriverManager
 import os.path
 import main
@@ -8,51 +9,43 @@ import Email
 import database_crud as db
 import pandas as pd
 
+chromeOption = options.Options()
+chromeOption.add_argument("--disable-dev-shm-usage")
+chromeOption.add_argument("--no-sandbox")
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options = chromeOption)
+
 def test_home_url():
-    driver = webdriver.Chrome(service=ChromeService(executable_path=ChromeDriverManager().install()))
     
     driver.get("http://127.0.0.1:5221/Main")
 
-    # Check that its at NLB Home Page
-    title = driver.title
-    assert title == "DevOps Team 5 Home Page"
-
+    # Check that its at Home Page
+    assert driver.title == "DevOps Team 5 Home Page"
 
 def test_upload_data_url():
-    driver = webdriver.Chrome(service=ChromeService(executable_path=ChromeDriverManager().install()))
     
     driver.get("http://127.0.0.1:5221/Upload_Data")
 
-    # Check that its at NLB Home Page
-    title = driver.title
-    assert title == "DevOps Team 5 Upload Data Page"
+    # Check that its at Upload Data Page
+    assert driver.title == "DevOps Team 5 Upload Data Page"
 
 def test_match_student_url():
-    driver = webdriver.Chrome(service=ChromeService(executable_path=ChromeDriverManager().install()))
-    
+ 
     driver.get("http://127.0.0.1:5221/Match_Student")
 
-    # Check that its at NLB Home Page
-    title = driver.title
-    assert title == "DevOps Team 5 Match Student Page"
+    # Check that its at Match Student Page
+    assert driver.title == "DevOps Team 5 Match Student Page"
 
-def test_prepare_email_url():
-    driver = webdriver.Chrome(service=ChromeService(executable_path=ChromeDriverManager().install()))
-    
+def test_prepare_email_url(): 
     driver.get("http://127.0.0.1:5221/Prepare_Email")
 
-    # Check that its at NLB Home Page
-    title = driver.title
-    assert title == "DevOps Team 5 Prepare Email Page"
+    # Check that its at Prepare Email Page
+    assert driver.title == "DevOps Team 5 Prepare Email Page"
 
 def test_settings_url():
-    driver = webdriver.Chrome(service=ChromeService(executable_path=ChromeDriverManager().install()))
-    
     driver.get("http://127.0.0.1:5221/Settings")
 
-    # Check that its at NLB Home Page
-    title = driver.title
-    assert title == "DevOps Team 5 Settings Page"
+    # Check that its at Settings Page
+    assert driver.title == "DevOps Team 5 Settings Page"
 
 def test_check_path_exists():
     check = main.check_path_exists('C:\\Windows')
@@ -89,3 +82,5 @@ def test_successful_upload_company_data():
     companyTestDataframe = pd.DataFrame(data = companyTestData)
     result = main.upload_data_func(companyTestDataframe, 'company')
     assert result == 'success'
+
+
